716fe5810b8acb689986f1e94db6f16c0ed02d80
Jorge Martin <jorge.martin@pulselive.com>
2022-09-01 15:54:37 +0200

    Changed layout to display 2 texts in a column

--- app/src/main/java/com/example/compose02basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/compose02basicscodelab/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.compose02basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -35,10 +35,10 @@ private fun MyApp() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello,")
+            Text(text = "$name!")
+        }
     }
 }
 
