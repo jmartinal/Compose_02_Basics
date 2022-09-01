daf04f332b15cb7609df60fba182ef6e2dc98a94
Jorge Martin <jorge.martin@pulselive.com>
2022-09-01 16:40:32 +0200

    Added "Show more" button

--- app/src/main/java/com/example/compose02basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/compose02basicscodelab/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,13 +45,16 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxWidth()
-        ) {
-            Text(text = "Hello,")
-            Text(text = "$name!")
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1F)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Show more")
+            }
         }
     }
 }
