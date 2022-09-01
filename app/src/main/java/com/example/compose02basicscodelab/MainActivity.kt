29bdf0c3c9f1f24b6c84f6d547576a3c0ea3b9ea
Jorge Martin <jorge.martin@pulselive.com>
2022-09-01 16:12:11 +0200

    Added multiple greetings

--- app/src/main/java/com/example/compose02basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/compose02basicscodelab/MainActivity.kt
@@ -26,9 +26,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp() {
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+        Column {
+            names.forEach { name ->
+                Greeting(name)
+            }
+        }
     }
 }
 
