fd871a96618d68db81019de59fcf8bd19042edba
Jorge Martin <jorge.martin@pulselive.com>
2022-09-02 12:40:32 +0200

    Updated designs to match requirements

--- app/src/main/java/com/example/compose02basicscodelab/ui/screen/Greeting.kt
+++ app/src/main/java/com/example/compose02basicscodelab/ui/screen/Greeting.kt
@@ -1,62 +1,38 @@
 package com.example.compose02basicscodelab.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose02basicscodelab.R
 import com.example.compose02basicscodelab.ui.theme.Compose02BasicsCodelabTheme
 
 @Composable
 fun Greeting(name: String) {
-    var isExpanded by rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (isExpanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1F)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                Text(
-                    text = "$name!",
-                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
-                )
-            }
-            OutlinedButton(
-                onClick = { isExpanded = !isExpanded }
-            ) {
-                Text(text = if (isExpanded) "Show less" else "Show more")
-            }
-        }
+        GreetingContent(name)
     }
 }
 
@@ -71,6 +47,46 @@ fun GreetingPreview() {
     Compose02BasicsCodelabTheme {
         Greeting(name = "Jorge")
     }
+}
+
+@Composable
+fun GreetingContent(name: String) {
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1F)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = "$name!",
+                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (isExpanded) {
+                Text(text = stringResource(R.string.lorem_ipsum))
+            }
+        }
+        IconButton(onClick = { isExpanded = !isExpanded }) {
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = stringResource(
+                    id = if (isExpanded) R.string.show_less else R.string.show_more
+                )
+            )
+        }
+    }
+
 }
 
 @Composable
